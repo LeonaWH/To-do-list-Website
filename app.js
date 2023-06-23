8d4f9f19687b8be63905ab607b7fbc4536892ad9
LeonaWH <leonavarghesewh@gmail.com>
2023-06-23 09:10:54 +0530

    Added Work list & About Page

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
+//jshint esverison:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
- 
+const date = require(__dirname+"/date.js");
+const ejs = require("ejs");
+
 const app = express();
- 
-let items = ["Buy Food", "Eat Food", "Cook Food"];
- 
+
+const items = ["Buy Food", "Eat Food", "Cook Food"];
+const workItems = [];
+
 app.set('view engine', "ejs");
- 
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static("public"));
- 
+app.use(express.static('public'));
+
 app.get("/", function (req, res) {
- 
-  var today = new Date();
- 
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
- 
-  let day = today.toLocaleDateString("en-US", options);
- 
- 
-  res.render("list", {
-    kindOfDay: day,
-    newListItems: items
-  });
- 
+
+  const day = date.getDate();
+  res.render("list", {listTitle: day, newListItems: items});
+
 });
- 
+
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
- 
-  items.push(item);
- 
-  res.redirect("/");
- 
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+
+  }
+
+
 });
- 
+
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
+
 app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+  console.log("Server started on Port 3000");
 });
